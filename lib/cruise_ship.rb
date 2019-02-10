9ccf724c401682455a786fa6b0c0d63016a086b8
veslie losius <veslielosius@yahoo.com>
2019-02-10 23:28:49 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,6 +6,10 @@
 # suite_e: "Crumpet the Elf"
 # }
 
-def select_winner(passengers)
-  # add the code snippet here!
+  select_winner(winner)
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
+winner
